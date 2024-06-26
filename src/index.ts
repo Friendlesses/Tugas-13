cd3c5308bedd6832cf956f8153b094401ab662ff
Friendlesses <krisumahendra@gmail.com>
2024-06-26 20:07:21 +0700

    Add user authentication endpoints

--- src/index.ts
+++ src/index.ts
@@ -49,4 +49,18 @@ app.get('/orders', (req: Request, res: Response) => {
     res.send('List of orders');
 });
 
+// Endpoint untuk register user
+app.post('/register', (req: Request, res: Response) => {
+    const { username, password } = req.body;
+    // Logika untuk register user
+    res.send('User registered');
+});
+
+// Endpoint untuk login user
+app.post('/login', (req: Request, res: Response) => {
+    const { username, password } = req.body;
+    // Logika untuk login user
+    res.send('User logged in');
+});
+
 //testing ada bug atau tidak
