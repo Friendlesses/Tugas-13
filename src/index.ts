da0c617fe56a5c5e75832b09f6d4e7097a85d24e
Friendlesses <krisumahendra@gmail.com>
2024-06-26 20:08:05 +0700

    Add product management endpoints

--- src/index.ts
+++ src/index.ts
@@ -49,4 +49,17 @@ app.get('/orders', (req: Request, res: Response) => {
     res.send('List of orders');
 });
 
+// Endpoint untuk menambah produk
+app.post('/products', (req: Request, res: Response) => {
+    const { name, price } = req.body;
+    // Logika untuk menambah produk
+    res.send('Product added');
+});
+
+// Endpoint untuk mendapatkan semua produk
+app.get('/products', (req: Request, res: Response) => {
+    // Logika untuk mendapatkan semua produk
+    res.send('List of products');
+});
+
 //testing ada bug atau tidak
